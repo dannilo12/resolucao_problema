486009914d897aeb5d47fef64d9fa0b8bf6640f1
danteking1@gmail.comclear <danteking1@gmail.com>
2022-09-24 00:29:34 -0300

    contando numeros

--- remote2.js
+++ remote2.js
@@ -1,5 +1,14 @@
 var alunos = 10
 
-for ( let cont = 0; cont < alunos; cont++ ){
-    console.log(cont)
+for ( let cont = 0; cont <= alunos; cont++ ){
+    //console.log(cont)
+
+    if(cont == 0){
+        console.log("O numero atual é zero!")
+    }else if (cont % 2 == 0){
+        console.log("O numero " + cont + " é par! ")
+    }else{
+        console.log("O numero " + cont + " é impar! ")
+    }
+
 }
