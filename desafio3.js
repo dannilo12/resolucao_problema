6ee17e4d2067ab18d0f80ace75da1d8c97a4caf0
danteking1@gmail.comclear <danteking1@gmail.com>
2022-09-23 23:34:11 -0300

    ajustando a quantidade de peças

---
+++ desafio3.js
@@ -0,0 +1,13 @@
+var listaDePecas = ["Amortecedor" , "Motor", "Filtro de ar"]
+
+let peso = 50
+if (peso < 100) {
+    console.log("A peça deve pesar no mínimo 100g")
+}else{
+    console.log("A peça tem opeso adequado")
+}
+if (listaDePecas.length < 10 ){
+    console.log("Ainda há espaço para cadastro!")
+}else{
+    console.log("Não há mais espaço para cadastro!")
+}
